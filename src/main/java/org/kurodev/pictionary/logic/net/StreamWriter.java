4ec3b1159e05440dfa2e797f60942a43e4bae4ec
kuro <dirkreher@ymail.com>
2020-08-19 12:20:34 +0200

    did some more work on network callback - this is just backing up data, so no significant progress

--- src/main/java/org/kurodev/pictionary/logic/net/StreamWriter.java
+++ src/main/java/org/kurodev/pictionary/logic/net/StreamWriter.java
@@ -1,13 +1,19 @@
 package org.kurodev.pictionary.logic.net;
 
+import org.kurodev.pictionary.logic.net.encoding.Code;
+import org.kurodev.pictionary.logic.net.encoding.Encodable;
+
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
  * @author kuro
  **/
 public class StreamWriter {
+    public static final byte[] DELIMITER = "\n\n\n".getBytes();
+    Charset set = StandardCharsets.UTF_8;
     private final OutputStream out;
 
     protected StreamWriter(OutputStream out) {
@@ -15,12 +21,25 @@ public class StreamWriter {
     }
 
     public void write(byte[] bytes) throws IOException {
-            out.write(bytes.length);
-            out.write(bytes);
-            out.flush();
+        out.write(bytes.length);
+        out.write(bytes);
+        out.flush();
     }
 
     public void write(String msg) throws IOException {
-        write(msg.getBytes(StandardCharsets.UTF_8));
+        write(msg.getBytes(set));
+    }
+
+    public void write(Encodable obj) throws IOException {
+        byte[] code = Code.get(obj).name().getBytes(set);
+        byte[] arr = new byte[obj.encode().length + code.length + DELIMITER.length];
+        System.arraycopy(code, 0, arr, 0, code.length);
+        System.arraycopy(DELIMITER, 0, arr, code.length, DELIMITER.length);
+        byte[] encode = obj.encode();
+        for (int i = 0; i < encode.length; i++) {
+            int x = i + code.length + DELIMITER.length;
+            arr[x] = encode[i];
+        }
+        write(arr);
     }
 }
