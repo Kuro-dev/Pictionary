f855cf54d8d4d6801922287cff50bd03814774c7
MCXIV <mcxiv14@gmail.com>
2020-08-23 15:02:24 +0530

    Added start menu sort of thing

---
+++ src/test/java/org/kurodev/pictionary/overlay/GUIGetInfoTest.java
@@ -0,0 +1,13 @@
+package org.kurodev.pictionary.overlay;
+
+import static org.junit.Assert.*;
+
+public class GUIGetInfoTest {
+
+    public static void main(String[] args) {
+
+        new GUIGetInfo();
+
+    }
+
+}
