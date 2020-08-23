4c009a32342539e41e92e70e092f6e42fc0b01ac
kuro <dirkreher@ymail.com>
2020-08-23 08:51:01 +0200

    introduced getters and setters

--- src/main/java/org/kurodev/pictionary/overlay/GUIManager.java
+++ src/main/java/org/kurodev/pictionary/overlay/GUIManager.java
@@ -1,6 +1,6 @@
 package org.kurodev.pictionary.overlay;
 
-import org.kurodev.pictionary.logic.net.communication.Participant;
+import org.kurodev.pictionary.logic.util.Participant;
 import org.kurodev.pictionary.overlay.factory.GBC;
 
 import javax.swing.*;
@@ -9,8 +9,9 @@ import java.util.ArrayList;
 
 public class GUIManager {
 
-    static ArrayList<Participant> participant_list;
     private static GUIBody instance;
+
+    static ArrayList<Participant> participant_list;
 
     public static void instantiate(String name) {
 
@@ -19,44 +20,59 @@ public class GUIManager {
 
         participant_list = new ArrayList<>();
 
-        addParticipant("Hey");
-        addParticipant("May");
-        addParticipant("ugtfdsadi");
-        addParticipant("JGfkhgsv");
-        addParticipant("sdugvbksjd");
+        addParticipant(name);
 
     }
 
+    static int index = 0;
+
     public static void addParticipant(String name) {
-
-        if (name.length() > 9) name = name.substring(0, 9);
-
-        Participant participant = new Participant(name, (int) (2000 * Math.random()));
+        Participant participant = new Participant(name, 0);
+        addParticipant(participant);
         participant_list.add(participant);
+    }
 
-        JLabel lab = new JLabel(participant_list.size() + ": ");
-        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(participant_list.size() - 1).setGridx(0).setAnchor(GBC.WEST).setInsets(new Insets(5, 20, 5, 5)));
+    public static void addParticipant(Participant participant) {
+
+        JLabel lab = new JLabel(index + 1 + ": ");
+        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(index).setGridx(0).setAnchor(GBC.WEST).setInsets(new Insets(5, 20, 5, 5)));
         instance.pan_lft_mid.add(lab);
 
         lab = new JLabel(participant.getName());
-        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(participant_list.size() - 1).setGridx(1).setAnchor(GBC.WEST).setWeightx(1).setInsets(new Insets(5, 0, 5, 5)));
+        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(index).setGridx(1).setAnchor(GBC.WEST).setWeightx(1).setInsets(new Insets(5, 0, 5, 5)));
         instance.pan_lft_mid.add(lab);
 
-        lab = new JLabel("" + participant.score);
-        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(participant_list.size() - 1).setGridx(2).setAnchor(GBC.EAST).setInsets(new Insets(5, 5, 5, 20)));
+        lab = new JLabel("" + participant.getScore());
+        instance.lay_pan_lft_mid.setConstraints(lab, new GBC().setGridy(index).setGridx(2).setAnchor(GBC.EAST).setInsets(new Insets(5, 5, 5, 20)));
         instance.pan_lft_mid.add(lab);
 
 //        instance.pan_lft_mid.repaint();
         instance.frame.pack();
+        index++;
 
     }
 
+    public static void updateScore(String name, int score) {
+        for (int i = 0; i < participant_list.size(); i++)
+            if (participant_list.get(i).getName().equals(name))
+                participant_list.get(i).setScore(score);
 
-    public static void sendChat(String name, String text) {
-        //TODO reimplement that
+        instance.pan_lft_mid.removeAll();
+
+        index = 0;
+        for (int i = 0; i < participant_list.size(); i++) {
+            addParticipant(participant_list.get(i));
+        }
     }
 
-    public static void updateScore(String name, int i) {
-        //TODO reimplement that
+    static String chat_text = "";
+
+    public static void sendChat(String name, String message) {
+
+        participant_list.stream().filter(participant -> participant.getName().equals(name)).findFirst().ifPresent(participant -> {
+            chat_text += "<BR /><FONT color=\"" + participant.getColour() + "\">" + name + ": " + message + "</FONT>";
+            instance.txt_scp_rht_mid.setText("<HTML><BODY>" + chat_text + "</HTML></BODY>");
+        });
+
     }
 }
