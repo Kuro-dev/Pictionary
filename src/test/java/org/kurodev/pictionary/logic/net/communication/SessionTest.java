fc4fed09a2493e1d5e716acedb3ec4fb8f09be4b
kuro <dirkreher@ymail.com>
2020-08-24 20:16:08 +0200

    setup test to check whether network communication works reliably also added comments to clarify what is happening added send method to HostSession.java

--- src/test/java/org/kurodev/pictionary/logic/net/communication/SessionTest.java
+++ src/test/java/org/kurodev/pictionary/logic/net/communication/SessionTest.java
@@ -1,14 +1,17 @@
 package org.kurodev.pictionary.logic.net.communication;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.kurodev.pictionary.logic.Pictionary;
+import org.kurodev.pictionary.logic.img.Pixel;
 import org.kurodev.pictionary.logic.net.encoding.Encodable;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class SessionTest {
 
@@ -32,26 +35,35 @@ public class SessionTest {
         }
         //wait for the connections
         if (!session.awaitConnections(3, TimeUnit.SECONDS)) {
-            Assert.fail("Timeout");
+            fail("Timeout");
         }
         //start the game
         session.start();
         //clients send some data to the host
         for (NetworkHandler client : clients) {
-            client.send(new Pictionary(client.toString()));
+            int randomData = client.hashCode();
+            client.send(new Pixel(randomData, randomData, randomData));
         }
         Encodable en = new Pictionary("HelloWorld!", 50);
         //host sends some data to the clients
         session.send(en);
-        Thread.sleep(200);
-        //check if the host received ALL the data sent from each client
-        for (NetworkHandler client : clients) {
-            Assert.assertTrue(hostCallback.getEncodables().contains(new Pictionary(client.toString())));
-        }
-        //check if each client received the data sent by the Host
-        for (VoidCallback clientCallback : clientCallbacks) {
-            Assert.assertTrue(clientCallback.getEncodables().contains(en));
-        }
+        //open new thread to simulate different devices
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("will never be thrown");
+            }
+            //check if the host received ALL the data sent from each client
+            for (NetworkHandler client : clients) {
+                int randomData = client.hashCode();
+                assertTrue(hostCallback.getEncodables().contains(new Pixel(randomData, randomData, randomData)));
+            }
+            //check if each client received the data sent by the Host
+            for (VoidCallback clientCallback : clientCallbacks) {
+                assertTrue(clientCallback.getEncodables().contains(en));
+            }
+        }).start();
     }
 
 }
