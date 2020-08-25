8a84a15ce17071e9e66ad32f508145c2fe8ff27e
kuro <dirkreher@ymail.com>
2020-08-25 21:44:10 +0200

    added comment with task for my boy

--- src/main/java/org/kurodev/pictionary/overlay/factory/ResourceManager.java
+++ src/main/java/org/kurodev/pictionary/overlay/factory/ResourceManager.java
@@ -6,7 +6,7 @@ import java.net.URL;
 public class ResourceManager {
     private static final int WIDTH = 200, HEIGHT = 200;
     private static final URL IMG = ResourceManager.class.getResource("/img/gameIcon.jpg");
-
+    //TODO, Please make resizable to the given width and height up above.
     public static Icon getIcon() {
         return new ImageIcon(IMG);
     }
