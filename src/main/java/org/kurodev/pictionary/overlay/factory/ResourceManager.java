686d943a0d670e0ada3e492aea396d1d3e9db058
kuro <dirkreher@ymail.com>
2020-08-25 21:43:24 +0200

    added imageIcon

--- src/main/java/org/kurodev/pictionary/overlay/factory/ResourceManager.java
+++ src/main/java/org/kurodev/pictionary/overlay/factory/ResourceManager.java
@@ -1,29 +1,14 @@
 package org.kurodev.pictionary.overlay.factory;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public class ResourceManager {
+    private static final int WIDTH = 200, HEIGHT = 200;
+    private static final URL IMG = ResourceManager.class.getResource("/img/gameIcon.jpg");
 
     public static Icon getIcon() {
-
-        BufferedImage image = null;
-
-        // Attempt to read image using InageIO.read from resources.
-
-        if (image == null) {
-            image = new BufferedImage(150, 60, BufferedImage.TYPE_INT_ARGB);
-            for (int i = 0; i < 150; i++) {
-                for (int j = 0; j < 60; j++) {
-                    int c = (int) (Math.random() * 255);
-                    image.setRGB(i, j, new Color(c, c, c).getRGB());
-                }
-            }
-        }
-
-        return new ImageIcon(image);
-
+        return new ImageIcon(IMG);
     }
 
 }
